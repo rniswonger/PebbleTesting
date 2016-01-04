eea4b8cf4f3a9d9423ebc89c8362d44b0f06a998
Ryan Niswonger <ryan.niswonger@gmail.com>
2016-01-03 18:52:33 -0800

    Add battery level text display and update

--- src/main.c
+++ src/main.c
@@ -5,13 +5,30 @@
 
 #include <pebble.h>
 static Window *s_main_window;
-static TextLayer *s_time_layer;
 static GFont s_time_font;
+static TextLayer *s_time_layer;
 static GFont s_weather_font;
 static TextLayer *s_weather_layer;
+static GFont s_battery_font;
+static int s_battery_level;
+static TextLayer *s_battery_layer;
 
 #define KEY_TEMPERATURE 0
 #define KEY_CONDITIONS 1
+
+static void battery_callback(BatteryChargeState state) {
+  s_battery_level = state.charge_percent;
+  
+  // write battery level into a buffer
+  static char s_buffer[8];
+  snprintf(s_buffer, sizeof(s_buffer), "%d%%", s_battery_level);
+  text_layer_set_text(s_battery_layer, s_buffer);
+  
+}
+
+static void battery_update_proc(Layer *layer, GContext *ctx) {
+  
+}
 
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
@@ -83,6 +100,7 @@ static void main_window_load(Window *window) {
   // Custom fonts
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ALIEN_REGULAR_40));
   s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_COND_REG_20));
+  s_battery_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_FFFHARMONY_8));
   
   // Create the time TextLayer
   s_time_layer = text_layer_create(
@@ -95,11 +113,12 @@ static void main_window_load(Window *window) {
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
-  // Add it as a child layer to the Window's root Layer
+  // Add time Layer as child layer to Window's root Layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   
   
-  // Create the temperature TextLayer
+  
+  // Create the weather TextLayer
   s_weather_layer = text_layer_create(
     GRect(0, PBL_IF_ROUND_ELSE(105, 100), bounds.size.w, 25)
   );
@@ -111,8 +130,24 @@ static void main_window_load(Window *window) {
   text_layer_set_text(s_weather_layer, "Loading...");
   text_layer_set_font(s_weather_layer, s_weather_font);
   
-  // Add weather Layer to Window's root Layer
+  // Add weather Layer as child layer to Window's root Layer
   layer_add_child(window_layer, text_layer_get_layer(s_weather_layer));
+  
+  
+  // Create battery TextLayer
+  s_battery_layer = text_layer_create(
+    GRect(0, PBL_IF_ROUND_ELSE(135, 130), bounds.size.w, 10)
+  );
+  
+  // Style battery text
+  text_layer_set_background_color(s_battery_layer, GColorClear);
+  text_layer_set_text_color(s_battery_layer, GColorDarkGray);
+  text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_battery_layer, "??%");
+  text_layer_set_font(s_battery_layer, s_battery_font);
+  
+  // Add battery layer as child layer to Window's root Layer
+  layer_add_child(window_layer, text_layer_get_layer(s_battery_layer));
   
 }
 
@@ -120,10 +155,12 @@ static void main_window_unload(Window *window) {
   // Destroy TextLayers
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_weather_layer);
+  text_layer_destroy(s_battery_layer);
   
   // Unload GFonts
   fonts_unload_custom_font(s_time_font);
   fonts_unload_custom_font(s_weather_font);
+  fonts_unload_custom_font(s_battery_font);
 }
 
 
@@ -146,13 +183,19 @@ static void init() {
   // Make sure the time is displayed when started
   update_time();
   
-  // Register callbacks
+  // Register weather callbacks
   app_message_register_inbox_received(inbox_received_callback);  
   app_message_register_inbox_dropped(inbox_dropped_callback);
   app_message_register_outbox_failed(outbox_failed_callback);
   app_message_register_outbox_sent(outbox_sent_callback);
   
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  
+  // Register battery callback
+  battery_state_service_subscribe(battery_callback);
+  
+  // Show battery at start
+  battery_callback(battery_state_service_peek());
 }
 
 static void deinit() {
